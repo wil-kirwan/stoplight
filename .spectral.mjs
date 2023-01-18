a1a74f1f2aa76ca3c75026848df6f8f5ea0d1ebc
Wil Kirwan <wil.kirwan@stoplight.io>
2023-01-18 09:29:56 -0600

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3ODc0MA/spectral.js?branch_slug=main&token=3dd89265-43bc-4d4e-90e9-00c2e6fd1349";
+export default { extends: ruleset };
